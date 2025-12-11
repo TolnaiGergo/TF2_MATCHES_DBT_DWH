0c1f40e054683225f081f97d02d9cf787a0763d2
TolnaiGergo <gergotolnai001@gmail.com>
2025-12-11 14:14:04 +0100

    added core map snapshot model

---
+++ models/core/v_stg/v_stg_map.sql
@@ -0,0 +1,32 @@
+
+with stg_map_cte as(
+    select
+        map_index,
+        map_name,
+        game_mode,
+         _stg_file_name,
+         _stg_file_load_ts,
+         _stg_copy_ts
+
+    from {{ ref('stg_map')}}
+),
+v_stg_map_cte as(
+    select
+       -- business key
+       map_index,
+       -- payload
+       map_name,
+       game_mode,
+       -- hash diff
+       {{ hash_cols('stg_map',['map_name','game_mode'],false,true)}} as hash_diff,
+       --metadata
+         _stg_file_name,
+         _stg_file_load_ts,
+         _stg_copy_ts
+    from stg_map_cte   
+)
+select
+    *
+from v_stg_map_cte   
+    
+    
