878f6e5a139dfeeed46ccba21db3af4ddd9d3418
TolnaiGergo <gergotolnai001@gmail.com>
2025-12-18 13:26:40 +0100

    removed hash diff

--- models/core/v_stg/v_stg_match.sql
+++ models/core/v_stg/v_stg_match.sql
@@ -102,9 +102,7 @@ v_stg_match_cte as(
             ELSE 'unknown'
         END AS leave_reason,
        reached_conclusion,
-       -- hash diff
-       {{ hash_cols('stg_match',['match_id','map_index','player_id','classes_played','_STG_FILE_NAME','_STG_FILE_LOAD_TS','_STG_COPY_TS'], true, true) }} as hash_diff,
-       --metadata
+      --metadata
        _stg_file_name,
        _stg_file_load_ts,
        _stg_copy_ts
