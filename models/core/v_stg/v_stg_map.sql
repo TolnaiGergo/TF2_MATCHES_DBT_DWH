044021e13f22bd15c43632af06132d7d5e41e278
TolnaiGergo <gergotolnai001@gmail.com>
2026-01-15 16:10:20 +0100

    added default rows for player and map models

--- models/core/v_stg/v_stg_map.sql
+++ models/core/v_stg/v_stg_map.sql
@@ -24,9 +24,36 @@ v_stg_map_cte as(
          _stg_file_load_ts,
          _stg_copy_ts
     from stg_map_cte   
+),
+default_rows_cte as (
+    select
+        map_index,
+        map_name,
+        game_mode,
+        hash_diff,
+        _stg_file_name,
+        _stg_file_load_ts,
+        _stg_copy_ts
+    from values
+        (0,
+        'unknown',
+        'unknown',
+        TO_BINARY('00000000000000000000000000000000', 'HEX'),
+        'no_source',
+        to_timestamp_ntz('1900-01-01 00:00:00'),
+        to_timestamp_ntz('1900-01-01 00:00:00'))
+    as t(map_index, map_name, game_mode, hash_diff, _stg_file_name, _stg_file_load_ts, _stg_copy_ts)
 )
 select
     v_stg .*
-from v_stg_map_cte as v_stg   
+from v_stg_map_cte as v_stg
+
+union
+
+select
+    default_rows_cte.*
+from default_rows_cte
+
+
     
     
--- models/core/v_stg/v_stg_player.sql
+++ models/core/v_stg/v_stg_player.sql
@@ -26,9 +26,36 @@ v_stg_player_cte as(
          _stg_file_load_ts,
          _stg_copy_ts
     from stg_player_cte   
+),
+default_rows_cte as (
+    select
+        player_id,
+        player_name,
+        country,
+        lvl,
+        hash_diff,
+        _stg_file_name,
+        _stg_file_load_ts,
+        _stg_copy_ts
+    from values
+        (0,
+        'unknown',
+        'unknown',
+        -1,
+        TO_BINARY('00000000000000000000000000000000', 'HEX'),
+        'no_source',
+        to_timestamp_ntz('1900-01-01 00:00:00'),
+        to_timestamp_ntz('1900-01-01 00:00:00'))
+    as t(player_id, player_name, country, lvl, hash_diff, _stg_file_name, _stg_file_load_ts, _stg_copy_ts)
 )
 select
     v_stg .*
-from v_stg_player_cte as v_stg   
+from v_stg_player_cte as v_stg
+
+union
+
+select
+    default_rows_cte.*
+from default_rows_cte
     
     
