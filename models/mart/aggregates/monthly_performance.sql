d719bbb58479a6eba8c6d434a680145985b9d1ad
TolnaiGergo <gergotolnai001@gmail.com>
2026-01-21 15:27:59 +0100

    added monthly performance aggregation table

---
+++ models/mart/aggregates/monthly_performance.sql
@@ -0,0 +1,26 @@
+with aggregated_match_cte as (
+    select
+        year(match_date) as year,
+        month(match_date) as month,
+        map_name,
+        count(match_id) as total_matches,
+        SUM(
+            CASE 
+                WHEN lower(winning_team) = lower(result_team)  THEN 1
+                ELSE 0
+            END
+        ) as total_wins,
+        (total_matches - total_wins) as total_loses,
+        avg(kills) as avg_kills,
+        avg(deaths) as avg_deaths,
+        avg(damage) as avg_damage
+
+    from {{ ref('fact_match')}}
+    inner join {{ ref('dim_map')}}
+        using(dim_map_sk)
+    group by year(match_date),
+             month(match_date),
+             map_name
+    order by year desc, month desc, map_name asc
+)
+select * from aggregated_match_cte
