9868939f8d639e72282c8576fbb3e9e1b7cdab3f
TolnaiGergo <gergotolnai001@gmail.com>
2026-07-21 15:22:23 +0200

    comments forever

--- models/core/incremental/match.sql
+++ models/core/incremental/match.sql
@@ -53,3 +53,5 @@ where match_id is not null
     and v_stg._stg_file_load_ts > ( select max(_stg_file_load_ts) from {{this}})
     and v_stg.match_id not in (select match_id from {{ this }})
 {% endif %}
+
+-- another comment
