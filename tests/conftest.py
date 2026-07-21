f454a0a8f1c29df9aee8620999fdb1e3a970c69d
TolnaiGergo <gergotolnai001@gmail.com>
2026-07-21 14:46:22 +0200

    asd

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,8 +14,8 @@ import subprocess
 import pytest
 
 
-REPO_ROOT = Path(__file__).resolve().parents[2]
-SQL_ROOT = REPO_ROOT / "TF2/models"
+REPO_ROOT = Path(__file__).resolve().parents[1]
+SQL_ROOT = REPO_ROOT / "models"
 
 def get_files_from_workdir(workdir: Path = SQL_ROOT, pattern: str = "*.sql", regex: re.Pattern | None = None) -> list[Path]:
     """
